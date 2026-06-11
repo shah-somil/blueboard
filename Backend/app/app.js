29fcf720ad21c5dd87884ef8a6a44ff385ae2d98
Somil <shah.som@northeastern.edu>
2026-06-11 00:43:34 -0400

    cors error

--- Backend/app/app.js
+++ Backend/app/app.js
@@ -5,16 +5,18 @@ import cookieParser from "cookie-parser";
 import * as authController from "./controllers/auth-controller.js";
 import "dotenv/config";
 import router from "./routes/index.js";
+const clientURL = process.env.CLIENT_URL || "http://localhost:3001";
+
 const corOptions = {
   credentials: true,
-  origin: "http://localhost:3001",
+  origin: clientURL,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
 const initialize = (app) => {
   app.use(cors(corOptions)); // use cors middleware
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header("Access-Control-Allow-Origin", clientURL);
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
